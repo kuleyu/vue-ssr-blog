cc88f15b4045bf076dc63ed23497207f5cc6fc6b
yangming <472102644@qq.com>
2018-05-04 11:58:15 +0800

    init

--- build.js
+++ build.js
@@ -13,7 +13,6 @@ handler.on('error', function (err) {
   console.error('Error:', err.message)
 })
 
-// 监听 push 事件
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
