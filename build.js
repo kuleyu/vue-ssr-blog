16fc167be35f12bf932820bd7d5853a7a3bb8e96
yangming <472102644@qq.com>
2018-05-04 11:35:19 +0800

    init

---
+++ build.js
@@ -0,0 +1,31 @@
+var http = require('http')
+var spawn = require('child_process').spawn
+var createHandler = require('github-Webhooks-handler')
+var handler = createHandler({ path: '/pushCode', secret: 'auto-build-blog' }) // 在代码仓库的 Webhooks 选项处配置
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location')
+  })
+}).listen(3000)
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+// 监听 push 事件
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+  rumCommand('sh', ['./build.sh'], function( txt ) { // 执行 autoBuild.sh 脚本文件
+    console.log(txt)
+  })
+})
+
+function rumCommand( cmd, args, callback ) {
+    var child = spawn( cmd, args )
+    var response = ''
+    child.stdout.on('data', function( buffer ){ response += buffer.toString(); })
+    child.stdout.on('end', function(){ callback( response ) })
+}
