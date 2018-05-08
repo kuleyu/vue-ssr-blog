041a1b4845466390f498c7fc8cffd1c64e17e6d3
yangming <472102644@qq.com>
2018-05-08 09:58:26 +0800

    deploy

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env sh
 set -e
 echo 'git pull ...'
-# git reset --hard origin/master
-# git clean -f
-# git pull
-# pm2 stop server
-# npm run start
+git reset --hard origin/master
+git clean -f
+git pull
+
+echo 'pm2 stop server ...'
+pm2 stop server
+
+echo 'npm run build ...'
+npm run build
+
+echo 'npm run build ...'
+npm run start
 # npm run www
