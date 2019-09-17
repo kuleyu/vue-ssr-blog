90645d36c01e50c61d8ede103ec4a83f4e638ede
yangming <472102644@qq.com>
2019-09-17 21:33:47 +0800

    fix

--- src/components/PageBottom.vue
+++ src/components/PageBottom.vue
@@ -195,7 +195,7 @@
 
       async handleGithub() {
         const loading = this.$loading({ text: '处理中...' })
-        const { data: { success, data: { login, node_id } } } = await axios.get('https://github.com/login/oauth/authorize?client_id=fd499caa8b7738da9ec4&redirect_uri=http://localhost:3000/oauth/redirect')
+        const { data: { success, data: { login, node_id } } } = await axios.get('https://github.com/login/oauth/authorize?client_id=fd499caa8b7738da9ec4&redirect_uri=https://iming.work/oauth/redirect')
         if (success) {
           await AV.User.logIn(login, node_id)
           this.CURRENT_USER()
